310a205ec6a4349bb715666244c2fc39f600a496
aysegulsari <sariaysegulsari@gmail.com>
2020-05-09 23:12:47 +0300

    provider or consumer option is added to sign up"

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
 
 class UserCreationForm(UserCreationForm):
 
     class Meta:
-        fields=('username','first_name','last_name','email','password1','password2')
-        model = get_user_model()
+        fields=('username','first_name','last_name','email','password1','password2','isProvider')
+        widgets = {
+            'yes_or_no': forms.RadioSelect
+        }
+        model = User
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.fields['username'].label='User Name'
         self.fields['email'].label="Email Address"
+        self.fields['isProvider'].label="Sign up as a provider"
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib import auth
 # Create your models here.
+BOOL_CHOICES = ((True, 'Yes'), (False, 'No'))
 
 class User(auth.models.User,auth.models.PermissionsMixin):
+    isProvider=models.BooleanField(choices=BOOL_CHOICES,blank=False,default=False)
 
     def __str__(self):
         return "@{}".format(self.username)
