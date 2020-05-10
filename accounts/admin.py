f66572400b46702979a6fd4d61bb4cd7b3af6942
aysegulsari <sariaysegulsari@gmail.com>
2020-05-10 19:02:38 +0300

    instead of inheriting user class, onetoone relationship is used

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import UserProfileInfo, User
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(UserProfileInfo)
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User
 # Create your models here.
 BOOL_CHOICES = ((True, 'Yes'), (False, 'No'))
 
-class User(auth.models.User,auth.models.PermissionsMixin):
+class UserProfileInfo(models.Model):
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     isProvider=models.BooleanField(choices=BOOL_CHOICES,blank=False,default=False)
 
     def __str__(self):
-        return "@{}".format(self.username)
+        return self.user.username
